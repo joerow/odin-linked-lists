4e9336ff4e0ae825b7ffc11dada938e3d9db726e
joerow <joerow@gmail.com>
2023-11-30 18:54:47 +0800

    add webpack

--- node.js
+++
@@ -1,6 +0,0 @@
-export default class Node {
-  constructor(value, nextNode) {
-    value === null ? (this.value = null) : (this.value = value);
-    nextNode === null ? (this.nextNode = null) : (this.nextNode = nextNode);
-  }
-}
---
+++ src/linkedList.js
@@ -0,0 +1,48 @@
+import Node from "./node";
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    //create a new node
+    var node = new Node(value);
+    //to store the current node
+    var current;
+    //if list is Empty add the element and make it head
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      //iterate to the end of the list
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+
+      //add node
+      current.nextNode = node;
+    }
+    this.size++;
+  }
+
+  prepend(value) {
+    //create a new node
+    var node = new Node(value);
+
+    //to store the old head
+    var oldHead;
+
+    //if list is Empty add the element and make it head
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      oldHead = this.head;
+      this.head = node;
+      node.nextNode = oldHead;
+    }
+    this.size++;
+  }
+}
